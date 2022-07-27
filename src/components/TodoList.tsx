df73fb109e1f9a64f8f8e4b16736f694ca516cb9
Bishnu Dhakal <dhkbishnu123@gmail.com>
2022-07-27 12:27:38 +0545

    this is component branch

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,67 +1,65 @@
-import React from 'react'
-import "./style.css"
-import {Todo} from "../Model"
-import SingleTodo from './SingleTodo';
-import { Droppable } from 'react-beautiful-dnd';
+import React from "react";
+import "./style.css";
+import { Todo } from "../Model";
+import SingleTodo from "./SingleTodo";
+import { Droppable } from "react-beautiful-dnd";
 
-interface Props{
-    todos:Todo[];
-    setTodos:React.Dispatch<React.SetStateAction<Todo[]>>;
-    completeTodos: Todo[];
-    setCompleteTodo:React.Dispatch<React.SetStateAction<Todo[]>>;
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  completeTodos: Todo[];
+  setCompleteTodo: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
-const TodoList = ({todos, setTodos}: Props) => {
+const TodoList = ({ todos, setTodos }: Props) => {
   return (
     // <div className='todos'>
     //    {todos.map(todo => (
     //     // <li>{todo.todo}</li>
-    //     <SingleTodo todo= {todo} 
+    //     <SingleTodo todo= {todo}
     //    key={todo.id} todos={todos}
     //    setTodos={setTodos} />
     //    ))}
 
     //  </div>
-  // );
+    // );
 
-  <div className='container'>
-    <Droppable droppableId='TodosList'>
-      {
-        (provided) => (
-          <div className='todos' ref={provided.innerRef}
-          {...provided.droppableProps}>
-          <span className='todosHeading'>Active Tasks</span>
-          {todos.map((todo) =>(
-            <SingleTodo todo={todo} todos={todos}
-             key={todo.id}
-             setTodos={setTodos} />
-          ))}
-    
-        </div>
-        )
-      }
-   
-    </Droppable>
-
-
-    <Droppable droppableId='TodosRemove'>
-      {
-        () =>(
-          <div className='todos remove'>
-      <span className='todosHeading'>Complete Tasks</span>
-      {todos.map((todo) =>(
-        <SingleTodo 
-        todo={todo}
-         todos={todos}
-         key={todo.id}
-         setTodos={setTodos} />
-      ))}
-   </div>
-        )
-      }
+    <div className="container">
+      <Droppable droppableId="TodosList">
+        {(provided) => (
+          <div
+            className="todos"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <span className="todosHeading">Active Tasks</span>
+            {todos.map((todo) => (
+              <SingleTodo
+                todo={todo}
+                todos={todos}
+                key={todo.id}
+                setTodos={setTodos}
+              />
+            ))}
+          </div>
+        )}
       </Droppable>
-   
-  </div>
-  
- );
+
+      <Droppable droppableId="TodosRemove">
+        {() => (
+          <div className="todos remove">
+            <span className="todosHeading">Complete Tasks</span>
+            {todos.map((todo) => (
+              <SingleTodo
+                todo={todo}
+                todos={todos}
+                key={todo.id}
+                setTodos={setTodos}
+              />
+            ))}
+          </div>
+        )}
+      </Droppable>
+    </div>
+  );
 };
-export default TodoList
+export default TodoList;
